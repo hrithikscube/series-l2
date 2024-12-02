458d6e275100b8be995e8af956e4e8c32c5d3d3f
hrithikscube <hrithik@scube.me>
2024-12-02 16:37:27 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Home from './Components/Pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import SeriesL2 from './Components/Pages/SeriesL2';
+import NotFound from './Components/Pages/NotFound';
 
 const App = () => {
   return (
     <Routes>
 
       <Route path='/' element={<SeriesL2 />} />
-      <Route path='/series-l2' element={<SeriesL2 />} />
       <Route path='/test' element={<Home />} />
+      <Route path='*' element={<NotFound />} />
 
 
     </Routes>
---
+++ src/Components/Pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-screen lg:text-base text-sm'>Page Not Found!</div>
+    )
+}
+
+export default NotFound
